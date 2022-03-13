76fe2cfd0aa9fd19471beedb022c5b1e0768d617
Bojin Li <bojin.li@foxmail.com>
2022-03-13 21:34:47 +0800

    New socialLinks api

--- config/link.ts
+++ config/link.ts
@@ -1,4 +1,4 @@
-import {Github, Props, Telegram, Twitter, Bilibili } from '@icons-pack/react-simple-icons'
+import {Github, Props, Telegram, Twitter, Bilibili, Sinaweibo} from '@icons-pack/react-simple-icons'
 import { FC } from 'react'
 
 export interface LinkProps {
@@ -15,14 +15,28 @@ export const socialLinks: LinkProps[] = [
     name: 'GitHub',
     link: 'https://github.com/xxxbrian',
     icon: Github,
-    apiUrl: 'https://api.spencerwoo.com/substats/?source=github&queryKey=xxxbrian',
+    apiUrl: 'https://api.swo.moe/stats/github/xxxbrian',
     color: '#24292f',
+  },
+  {
+    name: 'Instagram',
+    link: 'https://instagram.com/li.pochin',
+    icon: Telegram,
+    apiUrl: 'https://api.swo.moe/stats/instagram/li.pochin',
+    color: '#d7417b',
+  },
+  {
+    name: 'Weibo',
+    link: 'https://weibo.com/u/6128167899',
+    icon: Sinaweibo,
+    apiUrl: 'https://api.swo.moe/stats/weibo/6128167899',
+    color: '#e71f19',
   },
   {
     name: 'Telegram',
     link: 'https://t.me/bojinli',
     icon: Telegram,
-    apiUrl: 'https://api.spencerwoo.com/substats/?source=telegram&queryKey=',
+    apiUrl: 'https://api.swo.moe/stats/telegram/bojinli',
     color: '#2CA5E0',
     followerName: 'Add friend',
   },
@@ -30,14 +44,14 @@ export const socialLinks: LinkProps[] = [
     name: 'Twitter',
     link: 'https://twitter.com/bojinli',
     icon: Twitter,
-    apiUrl: 'https://api.spencerwoo.com/substats/?source=twitter&queryKey=bojinli',
+    apiUrl: 'https://api.swo.moe/stats/twitter/bojinli',
     color: '#1da1f2',
   },
   {
     name: 'Bilibili',
     link: 'https://space.bilibili.com/50051237',
     icon: Bilibili,
-    apiUrl: 'https://api.spencerwoo.com/substats/?source=bilibili&queryKey=50051237',
+    apiUrl: 'https://api.swo.moe/stats/bilibili/50051237',
     color: '#f25d8e',
     followerName: 'subscribers',
   },
