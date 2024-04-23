db7d2249f06460f4841e9c5b06fc0c81f4a830dd
Bojin Li <bojinxx@gmail.com>
2024-04-23 17:18:42 +1000

    fix: add current vistor in kv to get real last vistor

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -12,7 +12,19 @@ export const setLastVisitor = async (ipInfo: ipInfo) => {
   await kv.set("last_visitor", ipInfo);
 };
 
+export const setCurrentVisitor = async (ipInfo: ipInfo) => {
+  const lv = await kv.get<ipInfo>("current_visitor");
+  await kv.set("current_visitor", ipInfo);
+  if (lv) {
+    await kv.set("last_visitor", lv);
+  }
+};
 export const getLastVisitor = async () => {
   noStore();
   return await kv.get<ipInfo>("last_visitor");
+};
+
+export const getCurrentVisitor = async () => {
+  noStore();
+  return await kv.get<ipInfo>("current_visitor");
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { setLastVisitor } from "@/lib/kv";
+import { setCurrentVisitor } from "@/lib/kv";
 import countries from "@/lib/countries.json";
 
 function getIP(request: Request | NextRequest): string {
@@ -29,7 +29,7 @@ export async function middleware(request: NextRequest) {
   const countryInfo = countries.find((x) => x.cca2 === country);
   if (countryInfo) {
     const flag = countryInfo.flag;
-    await setLastVisitor({ ip, country, city, flag });
+    await setCurrentVisitor({ ip, country, city, flag });
   }
   return NextResponse.next();
 }
