39faff0e1b118ec493f07f29863ec76c4df30091
Bojin Li <bojinxx@gmail.com>
2024-04-26 18:17:05 +1000

    fix: migrate next/head to the Metadata API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Overpass, Old_Standard_TT, Overpass_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Head from "next/head";
 
 // import { cn } from "@/lib/utils";
 
@@ -21,8 +20,10 @@ const mono = Overpass_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Bojin Li",
-  description: "New personal website of Bojin Li",
+  title: "Bojin Li | 李帛锦",
+  description: "Bojin Li | 李帛锦",
+  keywords:
+    "Bojin Li, 李帛锦, Brian, Bojin, xxxbrian, bojinli, UNSW, lipochin, 3xbrian",
 };
 
 export default function RootLayout({
@@ -32,32 +33,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <title>Bojin Li | 李帛锦</title>
-        <meta name="description" content="Bojin Li | 李帛锦" />
-        <meta
-          name="keywords"
-          content="Bojin Li, 李帛锦, Brian, Bojin, xxxbrian, bojinli, UNSW, lipochin, 3xbrian"
-        ></meta>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-      </Head>
       <body className={`${sans.variable} ${serif.variable} ${mono.variable}`}>
         <ThemeProvider
           attribute="class"
